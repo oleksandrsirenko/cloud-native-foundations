03571ae01058620e28fc1de8621c6fbc83021a36
Oleksandr Sirenko <oleksa.sirenko@gmail.com>
2021-06-20 18:12:04 +0300

    Add logging logic and finilize the lesson 2 tasks within README

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, json
 from werkzeug.wrappers import response
+import logging
 
 app = Flask(__name__)
 
@@ -10,6 +11,9 @@ def status():
         status=200,
         mimetype="application/json"
     )
+    # log line
+    app.logger.info("Status request successfull")
+
     return response
 
 
@@ -26,12 +30,19 @@ def metrics():
         status=200,
         mimetype="application/json"
     )
+
+    # log line
+    app.logger.info("Metrics request successfull")
     return response
 
 
 @app.route("/")
 def hello():
+    # log line
+    app.logger.info("Main request successfull")
     return "Hello World!"
 
 if __name__ == "__main__":
+    # stream logs to app.log file
+    logging.basicConfig(filename="app.log", level=logging.DEBUG)
     app.run(host='0.0.0.0')
